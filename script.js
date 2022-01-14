25c1416dd8d84b2d2385677188a811e6ee7e9613
wdwd <wdwdwddwfeaewf@gmail.com>
2022-01-14 16:27:42 +0500

    test

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = +prompt('Введите число');
+let b = +prompt('Введите второе число');
+let perPrym = (a + b) *2;
+let ploshKvad = (a * b);
+
+function out(a,b) {
+    if (a == b) {
+        alert(perPrym)
+    } else (a != b) 
+        alert(ploshKvad)
+}
+
+out(a, b)
